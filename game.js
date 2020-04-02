8b450f015df5aad41a1d605bebd382d72e23c779
juliosant <juliosantiago025@gmail.com>
2020-04-02 17:08:23 -0300

    Add inimigo

--- game.js
+++ game.js
@@ -39,7 +39,7 @@ function preload() {
 
 
     this.load.spritesheet('player', 'assets/player.png', { frameWidth: 20, frameHeight: 20 });
-    this.load.spritesheet('enemy', 'assets/enemy.png', { frameWidth: 24, frameHeight: 25 });
+    this.load.spritesheet('enemy', 'assets/enemy.png', { frameWidth: 20, frameHeight: 20 });
     this.load.spritesheet('coin', 'assets/coin.png', { frameWidth: 25, frameHeight: 25 })
 }
 
@@ -71,11 +71,11 @@ function create() {
         [1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
-        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 4, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
         [1, 0, 1, 1, 1, 3, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
         [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
     platforms = this.physics.add.staticGroup();
@@ -137,29 +137,121 @@ function create() {
                 repeat: -1,
             });
             }
+            else if (tile === 3) {
+                coins = this.physics.add.sprite(x + 12.5, y + 12.5, 'coin');
+                this.anims.create({
+                    key: 'gira-gira',
+                    frames: this.anims.generateFrameNumbers('coin', {start:0, end: 9}),
+                    frameRate: 10,
+                    repeat: -1,
+                });
+                
+
+            }
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+            else if (tile === 4) {
+                enemy = this.physics.add.sprite(x + 12.5, y + 12.5, 'enemy');
+                enemy.setCollideWorldBounds(true);
+                this.anims.create({
+                    key: 'goLeft',
+                    frames: this.anims.generateFrameNumbers('enemy', {start:16, end: 23}),
+                    frameRate: 10,
+                    repeat: -1,
+                });
+                this.anims.create({
+                    key: 'goRight',
+                    frames: this.anims.generateFrameNumbers('enemy', { start: 24, end: 31 }),
+                    frameRate: 10,
+                    repeat: -1
+                });
+                this.anims.create({
+                    key: 'goUp',
+                    frames: this.anims.generateFrameNumbers('enemy', { start: 8, end: 15 }),
+                    frameRate: 10,
+                    repeat: -1
+                });
+                this.anims.create({
+                    key: 'goDown',
+                    frames: this.anims.generateFrameNumbers('enemy', { start: 0, end: 7 }),
+                    frameRate: 10,
+                    repeat: -1
+                });
+                enemy.direction = "DOWN";
+            }
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
         }
     }
 
     scoreText = this.add.text(25, 500, 'Score: 0', { fontSize: '20px', fill: '#ffa' });
 
-    //criar colisao entre o objeto player e o objeto platforms
+    //Colisões:
     this.physics.add.collider(player, platforms);
+    this.physics.add.collider(enemy, platforms)
+    this.physics.add.overlap(player, coins, pegaMoeda, null, this);
 
     //preenche o objeto de cursores com 4 propriedades: up, down, left, right
     cursors = this.input.keyboard.createCursorKeys();
+  
     
-    //criar cloisão entre coins e player
-    this.physics.add.overlap(player, coins, pegaMoeda, null, this);
 }
 
 
 function update() {
-//--------------ANIMAÇÃO DA MOEDA------------------------------
+//::::::::::::::::::::ANIMAÇÃO DA MOEDA::::::::::::::::::::
     if (coins.setVelocityX(0) && coins.setVelocityY(0)){
         coins.anims.play('gira-gira', true);
     }
 
+//::::::::::::::::::::ANIMAÇÃO DA INIMIGO::::::::::::::::::::
 
+    if(Math.floor(enemy.x -12.5)%25 === 0 && Math.floor(enemy.y -12.5)%25 === 0){
+        var enemyCol = Math.floor(enemy.x/25);
+        var enemyRow = Math.floor(enemy.y/25);
+        //alert(this.maze[enemyRow][enemyCol]);
+        var validPath = [];
+
+        if(this.maze[enemyRow][enemyCol-1] !== 1 && enemy.direction !== 'RIGHT'){
+            validPath.push('LEFT');
+        }
+        if(this.maze[enemyRow][enemyCol+1] !== 1 && enemy.direction !== 'LEFT'){
+            validPath.push('RIGHT');
+        }
+        if(this.maze[enemyRow-1][enemyCol] !== 1 &&  enemy.direction !== 'DOWN'){
+            validPath.push('UP');
+        }
+        if(this.maze[enemyRow+1][enemyCol] !== 1 && enemy.direction !== 'UP'){
+            validPath.push('DOWN');
+        }
+        
+        enemy.direction = validPath[Math.floor(Math.random()*validPath.length)];
+    }
+        
+    switch(enemy.direction){
+		case 'LEFT':
+            enemy.x -= 1;
+            enemy.anims.play('goLeft', true);
+			break;
+		case 'RIGHT':
+			enemy.x += 1;
+            enemy.anims.play('goRight', true);
+			break;
+		case 'UP':
+			enemy.y -= 1;
+            enemy.anims.play('goUp', true)
+			break;
+		case 'DOWN':
+			enemy.y += 1;
+            enemy.anims.play('goDown', true)
+			break;
+			
+		}
+
+    
+
+
+//::::::::::::::::::::ANIMAÇÃO DO PLAYER::::::::::::::::::::
     player.setVelocityX(0);
     player.setVelocityY(0);
 
